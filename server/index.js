42cbb616cc744407f394f78368a5df4c66dc46fe
Suresh Kumar <sureshkumar79061@gmail.com>
2025-03-30 22:36:14 +0530

    Project Initialised

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+
+
+
+const PORT = process.env.PORT || 3000 ;
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+})
