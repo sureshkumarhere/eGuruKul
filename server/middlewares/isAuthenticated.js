981f732622a8648f95cc92c8b1d4830168d8a580
Akash Kumar Gaud <142931881+iamakashkumar09@users.noreply.github.com>
2025-07-16 23:01:47 +0530

    Added Frontend and Stripe for Payment

--- server/middlewares/isAuthenticated.js
+++ server/middlewares/isAuthenticated.js
@@ -1,9 +1,9 @@
 import jwt from "jsonwebtoken";
 
+
 const isAuthenticated = async (req, res, next) => {
   try {
     const token = req.cookies.token;
-    // console.log(token)
     if (!token) {
       return res.status(401).json({
         message: "User not authenticated",
@@ -17,12 +17,10 @@ const isAuthenticated = async (req, res, next) => {
         success: false,
       });
     }
-    req.id = decode.id;
-    // console.log("User ID:", decode.id);
+    req.id = decode.userId;
     next();
   } catch (error) {
     console.log(error);
   }
 };
-
 export default isAuthenticated;
